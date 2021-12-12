d5dee7b38dd974c8d56fa4473f440d8904a77062
Parbez <74945038+imranbarbhuiya@users.noreply.github.com>
2021-12-12 07:30:21 +0530

    few more examples added

--- main.py
+++ main.py
@@ -1,15 +1,36 @@
-# import csv
-#
-# with open("weather_data.csv") as data_file:
-#     data = csv.reader(data_file)
-#     temperatures = []
-#     for row in data:
-#         if row[1] != "temp":
-#             temperatures.append(int(row[1]))
-#     print(temperatures)
-
 import pandas
 
-data = pandas.read_csv("weather_data.csv")
-print(data)
-print(data["temp"])
+# data = pandas.read_csv("weather_data.csv")
+# data_dict = data.to_dict()
+# print(data_dict)
+#
+# temp_list = data["temp"].to_list()
+# print(temp_list)
+
+# Mean temperature
+# average_temp = sum(temp_list) / len(temp_list)
+# print(average_temp)
+
+# Mean using pandas
+# mean = data["temp"].mean()
+# print(mean)
+
+# highest number
+# highest = data["temp"].max()
+# print(highest)
+
+# get a row
+# row = data[data.day == "Monday"]
+# print(row)
+
+# Create a dataframe
+data_dict = {
+    "day": ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"],
+    "temp": [1, 2, 3, 4, 5],
+    "humidity": [1, 2, 3, 4, 5]
+}
+
+data = pandas.DataFrame(data_dict)
+# print(data)
+
+data.to_csv("new_data.csv")
